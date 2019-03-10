d0eea0363ea6db9fcd104af12ba801681ea231dd
Blai Samitier <blai.samitier@gmail.com>
2019-03-10 21:12:22 +0100

    build editor & build lib scripts

---
+++ src/webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+
+module.exports = {
+	entry: './src/pixel-icon.ts',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js']
+	},
+	output: {
+		filename: 'pixel-icon.js',
+		path: path.resolve(__dirname, '../lib')
+	}
+}
